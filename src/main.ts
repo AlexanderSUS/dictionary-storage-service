dd10d44e84af44a8d7fe460cd0e2b6ba0e94e426
Alexander Suslov <sasha_suslov07@mail.ru>
2022-09-17 17:04:02 +0300

    feat: install swagger

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import { AppModule } from './app.module';
 import 'dotenv';
 import 'reflect-metadata';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const port = process.env.PORT || 4000;
 
   const app = await NestFactory.create(AppModule);
+
+  const docsConfig = new DocumentBuilder().setTitle('Dictionary-store').build();
+
+  const document = SwaggerModule.createDocument(app, docsConfig);
+  SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
 
