eabcee86e1331947376e6154473eac7ccddaf3e1
Alexander Suslov <sasha_suslov07@mail.ru>
2023-07-07 01:17:16 +0300

    refactor: words service

---
+++ src/words/words.repository.ts
@@ -0,0 +1,129 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ModifiedUserWordDto } from './dto/modified-user-word.dto';
+import { UserWord } from './entities/userWord.entity';
+import QueryDto from './dto/query.dto';
+import getPartOfSpeechCriteria from 'src/utils/getPartOfSpeechCriteria';
+import { UpdateWordDto } from './dto/update-word.dto';
+import { Word } from './entities/word.entity';
+
+@Injectable()
+export class UserWordsRepository {
+  constructor(
+    @InjectRepository(ModifiedUserWordDto)
+    private readonly userWordsRepository: Repository<UserWord>,
+  ) {}
+
+  async create(word: Word, userId: string) {
+    const newWord = new UserWord();
+    newWord.word = word;
+    newWord.user.id = userId;
+
+    return await this.userWordsRepository.save(newWord);
+  }
+
+  async find(query: QueryDto, userId: string) {
+    const { status, exclude, include, order, limit, offset } = query;
+
+    const words = await this.userWordsRepository.find({
+      where: {
+        user: {
+          id: userId,
+        },
+        status,
+        word: {
+          partOfSpeech: getPartOfSpeechCriteria(exclude, include),
+        },
+      },
+      order: {
+        word: {
+          word: order,
+        },
+      },
+      relations: {
+        word: true,
+      },
+      skip: offset ? parseInt(offset) : 0,
+      take: limit ? parseInt(limit) : 10,
+    });
+
+    if (!words.length) {
+      throw new NotFoundException('Words not found');
+    }
+
+    return words;
+  }
+
+  async findByWord(word: string, userId: string) {
+    return this.userWordsRepository.findOne({
+      where: {
+        word: {
+          word,
+        },
+        user: {
+          id: userId,
+        },
+      },
+    });
+  }
+
+  async findOneById(wordId: string, userId: string) {
+    const word = await this.userWordsRepository.findOne({
+      where: {
+        user: {
+          id: userId,
+        },
+        id: wordId,
+      },
+      relations: {
+        word: true,
+      },
+    });
+
+    if (!word) {
+      throw new NotFoundException('Word not found');
+    }
+
+    return word;
+  }
+
+  async update(wordId: string, updateWordDto: UpdateWordDto, userId: string) {
+    const word = await this.userWordsRepository.findOne({
+      where: {
+        user: {
+          id: userId,
+        },
+        id: wordId,
+      },
+    });
+
+    if (!word) {
+      throw new NotFoundException('Word not found');
+    }
+
+    const updatedWord = await this.userWordsRepository.save({
+      ...word,
+      ...updateWordDto,
+    });
+
+    return updatedWord;
+  }
+
+  async remove(wordId: string, userId: string) {
+    const word = await this.userWordsRepository.findOne({
+      where: {
+        user: {
+          id: userId,
+        },
+        id: wordId,
+      },
+    });
+
+    if (!word) {
+      throw new NotFoundException('Word not found');
+    }
+
+    await this.userWordsRepository.remove(word);
+  }
+}
--- src/words/words.service.ts
+++ src/words/words.service.ts
@@ -3,100 +3,53 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import getFindOneOptionsByUserId from 'src/utils/getFindOneWordOptionsByUserId';
-import getPartOfSpeechCriteria from 'src/utils/getPartOfSpeechCriteria';
 import modifyUserWord from 'src/utils/modifyUserWord';
 import { WordsStorageService } from 'src/words-storage/words-storage.service';
-import { Repository } from 'typeorm';
 import { CreateWordDto } from './dto/create-word.dto';
 import { ModifiedUserWordDto } from './dto/modified-user-word.dto';
 import QueryDto from './dto/query.dto';
 import { UpdateWordDto } from './dto/update-word.dto';
-import { UserWord } from './entities/userWord.entity';
+import { UserWordsRepository } from './words.repository';
 
 @Injectable()
 export class WordsService {
   constructor(
     private readonly wordsStorage: WordsStorageService,
-    @InjectRepository(UserWord)
-    private userWordRepository: Repository<UserWord>,
+    private readonly userWordRepository: UserWordsRepository,
   ) {}
 
   async create(
-    { word }: CreateWordDto,
+    createWordDto: CreateWordDto,
     userId: string,
   ): Promise<ModifiedUserWordDto> {
-    const userWord = await this.userWordRepository.findOne(
-      getFindOneOptionsByUserId(word, userId),
+    const userWord = await this.userWordRepository.findByWord(
+      createWordDto.word,
+      userId,
     );
 
     if (userWord) {
       throw new ConflictException('This word already in your store');
     }
 
-    const newWord = await this.wordsStorage.getWordFromDb(word);
+    const newWord = await this.wordsStorage.getWordFromDb(createWordDto.word);
 
     if (!newWord) {
       throw new NotFoundException('Word not found');
     }
 
-    const data = await this.userWordRepository.save({
-      user: { id: userId },
-      word: newWord,
-    });
+    const data = await this.userWordRepository.create(newWord, userId);
 
     return modifyUserWord(data);
   }
 
   async findAll(query: QueryDto, id: string): Promise<ModifiedUserWordDto[]> {
-    const { status, exclude, include, order, limit, offset } = query;
-
-    const words = await this.userWordRepository.find({
-      where: {
-        user: {
-          id,
-        },
-        status,
-        word: {
-          partOfSpeech: getPartOfSpeechCriteria(exclude, include),
-        },
-      },
-      order: {
-        word: {
-          word: order,
-        },
-      },
-      relations: {
-        word: true,
-      },
-      skip: offset ? parseInt(offset) : 0,
-      take: limit ? parseInt(limit) : 10,
-    });
-
-    if (!words.length) {
-      throw new NotFoundException('Words not found');
-    }
+    const words = await this.userWordRepository.find(query, id);
 
     return words.map((word) => modifyUserWord(word));
   }
 
   async findOne(wordId: string, userId: string): Promise<ModifiedUserWordDto> {
-    const word = await this.userWordRepository.findOne({
-      where: {
-        user: {
-          id: userId,
-        },
-        id: wordId,
-      },
-      relations: {
-        word: true,
-      },
-    });
-
-    if (!word) {
-      throw new NotFoundException('Word not found');
-    }
+    const word = await this.userWordRepository.findOneById(wordId, userId);
 
     return modifyUserWord(word);
   }
@@ -106,41 +59,21 @@ export class WordsService {
     updateWordDto: UpdateWordDto,
     userId: string,
   ): Promise<ModifiedUserWordDto> {
-    const word = await this.userWordRepository.findOne({
-      where: {
-        user: {
-          id: userId,
-        },
-        id: wordId,
+    const word = await this.userWordRepository.findOneById(wordId, userId);
+
+    const updatedWord = await this.userWordRepository.update(
+      word.id,
+      {
+        ...word,
+        ...updateWordDto,
       },
-    });
-
-    if (!word) {
-      throw new NotFoundException('Word not found');
-    }
-
-    const updatedWord = await this.userWordRepository.save({
-      ...word,
-      ...updateWordDto,
-    });
+      userId,
+    );
 
     return modifyUserWord(updatedWord);
   }
 
   async remove(wordId: string, userId: string) {
-    const word = await this.userWordRepository.findOne({
-      where: {
-        user: {
-          id: userId,
-        },
-        id: wordId,
-      },
-    });
-
-    if (!word) {
-      throw new NotFoundException('Word not found');
-    }
-
-    await this.userWordRepository.remove(word);
+    await this.userWordRepository.remove(wordId, userId);
   }
 }
