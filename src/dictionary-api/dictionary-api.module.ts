f8592a048682d9a533c695797410710a25679e6c
Alexander Suslov <sasha_suslov07@mail.ru>
2023-07-07 23:29:48 +0300

    refactor: remove unused imports

--- src/dictionary-api/dictionary-api.module.ts
+++ src/dictionary-api/dictionary-api.module.ts
@@ -1,11 +1,9 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Word } from 'src/words/entities/word.entity';
 import { DictionaryApiService } from './dictionary-api.service';
 
 @Module({
-  imports: [HttpModule, TypeOrmModule.forFeature([Word])],
+  imports: [HttpModule],
   providers: [DictionaryApiService],
   exports: [DictionaryApiService],
 })
